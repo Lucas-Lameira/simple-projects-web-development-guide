9f6610475aa553606e75fd958e020da79e6509c5
Lucas de souza lameira <lucasslameira@gmail.com>
2020-09-11 05:08:53 -0300

    Front-end

--- Color cycle/script.js
+++ Color cycle/script.js
@@ -1,101 +1,85 @@
 const buttonElement = document.getElementById('botao');
 const boxColor = document.getElementById('box-color');
-let toggleButton = false;
-let hexaColor;
 
-let incrementRed = document.getElementById('incrementRed');
-let incrementGreen = document.getElementById('incrementGreen');
-let incrementBlue = document.getElementById('incrementBlue');
+let toggleButton = false;
 
 let redInput = document.getElementById('red');
 let greenInput = document.getElementById('green');
 let blueInput = document.getElementById('blue');
 
+let incrementRed = document.getElementById('incrementRed');
+let incrementGreen = document.getElementById('incrementGreen');
+let incrementBlue = document.getElementById('incrementBlue');
+
 let red;
 let green;
-let blue;
+let blue; 
 
-function play() {
-    //se os numeros forem precedidos de 0x o javascript interpreta como hexadecimal e converte para numeros inteiros
+let plusRed;
+let plusGreen;
+let plusBlue;
+
+function play() {    
     red = Number('0x' + redInput.value);
     green = Number('0x' + greenInput.value);
     blue = Number('0x' + blueInput.value);
-    console.log(red)
-    
-    checkIsNotNumber();
 
-    //valor em decimal
-   /*  console.log(r);
-    console.log(g);
-    console.log(b); */
-
-    //valor em hexadecimal
-    /* console.log(r.toString(16))
-    console.log(g.toString(16))
-    console.log(b.toString(16)) */
-
-
-    let y = red + Number('0x' + incrementRed.value);
-    
-    console.log(y)
-    console.log(y.toString(16));
-
-    if(toggleButton){
+    plusRed = Number('0x' + incrementRed.value);
+    plusGreen = Number('0x' + incrementGreen.value);
+    plusBlue = Number('0x' + incrementBlue.value);
+       
+    if(!toggleButton){
         buttonElement.innerHTML = 'Pause';
-        toggleButton = false;        
-        hexaColor = redInput.value + greenInput.value + blueInput.value;              
-        console.log(hexaColor);
-        boxColor.style.backgroundColor = '#' + hexaColor;        
+        toggleButton = true;        
     }else{
-        toggleButton = true;
-        buttonElement.innerHTML = 'Play'
+        buttonElement.innerHTML = 'Play'    
+        toggleButton = false;    
     }
 }
-
 
 function checkIsNotNumber() {
-    if(isNaN(red)){
-        red = '00';
-        /* console.log(`Red: ${red}`) */
-    }
-    if(isNaN(green)){
-        green = '00';
-    }
-    if(isNaN(blue)){
-        blue = '00';
-    }  
+    //isNaN() retorna true or false
+    if(isNaN(red)) red = 0;
+    if(isNaN(green)) green = 0;
+    if(isNaN(blue)) blue = 0;
+    if(isNaN(plusRed)) plusRed = 0;
+    if(isNaN(plusGreen)) plusGreen = 0;
+    if(isNaN(plusBlue)) plusBlue = 0;
 }
 
-/* 
-    valor(supondo que é hexa) > transformar em binario> incrementar o tamanho > converter em hexa
+function increments(){
+    red = red + plusRed;
+    green += plusGreen;
+    blue += plusBlue;  
+}
 
-  isNaN() true or false
-  
+function validadeMaxLenght(){
+    if(red > 255) red -= 255;    
+    if(green > 255) green -= 255;    
+    if(blue>255) blue -= 255;    
+}
 
+//os ifs dessa função são para a concatenação, que gera 
+function convertToHexadecimal() {
+    red = red.toString(16)
+    
+    if(red == 0) red = "00";
+    if(red < 9 && red > 0) red = "0"+ red;
+    if(red.length < 2) red = "0" + red;
 
-*/
-/* 
-function checkHexa () {
-    try {
-        
-    } catch(err) {
+    green = green.toString(16)
+    if(green == 0) green = "00";
+    if(green < 9 && green > 0) green = "0"+ green;
+    if(green.length < 2) green = "0" + green;
 
-    }
-} */
+    blue = blue.toString(16);
+    if(blue == 0) blue = "00";
+    if(blue < 9 && blue > 0) blue = "0"+ blue;
+    if(blue.length < 2) blue = "0" + blue;
+}
 
-
-
-/* 
-#1
-    input value from an event  
-
-#2
-    the boolean switch
-
-
-
-
-    */
-
-
-    /* */
+function converteToDecimal() {
+    red = Number('0x' + red);
+    green = Number('0x' + green);
+    blue = Number('0x' + blue);   
+}
